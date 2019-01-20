4ab674fff6627a5f2a3d2cb5434ff16e921bc208
Khalfan <khalfanawan2013@gmail.com>
2019-01-20 23:40:48 +0500

    null:1 GET http://localhost:8100/null 404 (Not Found) Image (async)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,11 +10,12 @@ import * as WC from 'woocommerce-api';
 export class HomePage {
 
   WooCommerce: any;
+  products: any[];
   constructor(public navCtrl: NavController) {
 
     this.WooCommerce = WC({
     
-      url: 'http://localhost/', // Your store URL
+      url: 'http://localhost', // Your store URL
       consumerKey: 'ck_8b45648647e2c1c8e5411d8674b4e88f7708842c', // Your consumer key
       consumerSecret: 'cs_0e2023161c545ea747f5a7e8751bfad6acd71f40', // Your consumer secret
       wpAPI: true, // Enable the WP REST API integration
@@ -23,7 +24,10 @@ export class HomePage {
     });
 
       this.WooCommerce.getAsync("products").then ( 
-      (data) => { console.log(data); }, 
+      (data) => { 
+       this.products=JSON.parse(data.body);
+       console.log(this.products);
+      }, 
       (err) => { console.log(err); } ); 
 
 
