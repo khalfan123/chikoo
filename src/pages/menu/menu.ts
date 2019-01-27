b2c8970335860d23ab50bbc0656769cc827f7a37
Khalfan <khalfanawan2013@gmail.com>
2019-01-27 16:43:48 +0500

    Display categories in menu tab

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from "../home/home";
-
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-menu',
@@ -9,9 +9,47 @@ import { HomePage } from "../home/home";
 })
 export class MenuPage {
   homePage: typeof HomePage;
+  WooCommerce: any;
+  categories: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage=HomePage;
+    this.categories = [];
+
+    this.WooCommerce = WC({
+    
+      url: 'http://localhost', // Your store URL
+      consumerKey: 'ck_8b45648647e2c1c8e5411d8674b4e88f7708842c', // Your consumer key
+      consumerSecret: 'cs_0e2023161c545ea747f5a7e8751bfad6acd71f40', // Your consumer secret
+      wpAPI: true, // Enable the WP REST API integration
+      version: 'wc/v3', // WooCommerce WP REST API version
+      
+    });
+
+    // Fetch the products from WooCommerce store
+    this.WooCommerce.getAsync("products/categories").then ( 
+    (data) => { 
+      console.log(JSON.parse(data.body));
+
+      //Declaring local variable to get the list of categories.
+      let temp: any [] = JSON.parse(data.body);
+      for (let i = 0; i < temp.length; i++) {
+        if (temp[i].parent == 0) {
+          if (temp[i].slug == "vegetables") {
+            temp[i].icon = "pizza";
+          }
+          if (temp[i].slug == "generalproducts") {
+            temp[i].icon = "basket";
+          }
+          this.categories.push(temp[i]);
+        }
+        
+        
+      }
+      
+    }, 
+    (err) => { console.log(err); } ); 
+
   }
 
   ionViewDidLoad() {
